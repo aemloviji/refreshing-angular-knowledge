9e4a927d28e8d8630b0796f0b182d55575b1734e
aemloviji <aemloviji@gmail.com>
2023-11-05 09:44:52 +0400

    add navigation using events

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css'],
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is simply a test',
+      'https://assets.bonappetit.com/photos/64349ba03fd52da4745a35f4/1:1/w_3563,h_3563,c_limit/04102023-ratatouille-lede.jpg'
+    ),
+  ];
+}
